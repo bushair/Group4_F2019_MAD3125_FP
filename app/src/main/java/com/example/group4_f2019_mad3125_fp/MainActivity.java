dcd07bbe1b61b3644e9e07894c34f5c207501da4
shiji703 <shijieugin3@gmail.com>
2019-12-10 08:26:12 -0500

    cancel label

--- app/src/main/java/com/example/group4_f2019_mad3125_fp/MainActivity.java
+++ app/src/main/java/com/example/group4_f2019_mad3125_fp/MainActivity.java
@@ -110,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
     {
         final Dialog dialog = new Dialog(this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        
+        dialog.setCancelable(false);
     }
 }
 
