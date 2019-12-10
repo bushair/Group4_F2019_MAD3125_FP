75526e61aa86e9d0a61f2abd15e43d09018c54bf
macstudent <bushairkm007@gmail.com>
2019-12-09 20:00:21 -0500

    navigation view

--- app/src/main/java/com/example/group4_f2019_mad3125_fp/MainActivity.java
+++ app/src/main/java/com/example/group4_f2019_mad3125_fp/MainActivity.java
@@ -76,4 +76,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void setUpNavigationView() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
+
 }
