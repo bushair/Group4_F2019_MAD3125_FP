98a56c343b7ecd763ec59d16e2d1ed7eca727eb3
shiji703 <shijieugin3@gmail.com>
2019-12-10 08:00:58 -0500

    payroll adaptor

--- app/src/main/java/com/example/group4_f2019_mad3125_fp/PayrollAdaptor.java
+++ app/src/main/java/com/example/group4_f2019_mad3125_fp/PayrollAdaptor.java
@@ -1,4 +1,25 @@
 package com.example.group4_f2019_mad3125_fp;
 
-public class PayrollAdaptor {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class PayrollAdaptor extends RecyclerView.Adapter<PayrollAdapter.ViewHolder>
+{
+    private Context context;
+    private List<PayrollData> payrollDataList;
+    private OnPayrollClicked onPayrollClicked;
+
+    public PayrollAdapter(Context context, List<PayrollData> payrollDataList, OnPayrollClicked onPayrollClicked) {
+        this.context = context;
+        this.payrollDataList = payrollDataList;
+        this.onPayrollClicked = onPayrollClicked;
+    }
+
+
 }
