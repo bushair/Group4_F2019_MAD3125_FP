943ac61d566e9f8aa5ca252f28e29cdb4fe90776
macstudent <bushairkm007@gmail.com>
2019-12-09 19:40:55 -0500

    login activity

--- app/src/main/java/com/example/group4_f2019_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/example/group4_f2019_mad3125_fp/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.group4_f2019_mad3125_fp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onLoginClick(View view){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
