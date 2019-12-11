ced3b02884c34149f765e319a17c9decdc4654b5
bushairkm007 <bushairkm007@gmail.com>
2019-12-11 16:33:22 -0500

    changes

--- app/src/main/java/com/example/group4_f2019_mad3125_fp/ui/addEmployee/AddEmployeeFragment.java
+++ app/src/main/java/com/example/group4_f2019_mad3125_fp/ui/addEmployee/AddEmployeeFragment.java
@@ -12,100 +12,95 @@ import android.view.ViewGroup;
 
 import com.example.group4_f2019_mad3125_fp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AddEmployeeFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AddEmployeeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.payrollsystem.R;
+
 public class AddEmployeeFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    RadioGroup rg_part_time, vehicle_layout;
+    LinearLayout part_time_layout, intern_layout, full_time_layout, vehicle_details_layout;
+    RadioButton rb_commission_based, rb_fixed_based, rb_part_time, rb_intern, rb_full_time;
+    CheckBox has_vehicle_check;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_add_employee, container, false);
+        has_vehicle_check = root.findViewById(R.id.has_vehicle_check);
+        rb_commission_based = root.findViewById(R.id.rb_commission_based);
+        rb_fixed_based = root.findViewById(R.id.rb_fixed_based);
+        rb_part_time = root.findViewById(R.id.rb_part_time);
+        rb_intern = root.findViewById(R.id.rb_intern);
+        rb_full_time = root.findViewById(R.id.rb_full_time);
+        part_time_layout = root.findViewById(R.id.part_time_layout);
+        intern_layout = root.findViewById(R.id.intern_layout);
+        full_time_layout = root.findViewById(R.id.full_time_layout);
+        rg_part_time = root.findViewById(R.id.rg_part_time);
+        vehicle_layout = root.findViewById(R.id.vehicle_layout);
+        vehicle_details_layout = root.findViewById(R.id.vehicle_details_layout);
 
-    private OnFragmentInteractionListener mListener;
+        has_vehicle_check.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    vehicle_layout.setVisibility(View.VISIBLE);
+                    vehicle_details_layout.setVisibility(View.VISIBLE);
+                }
+                else {
+                    vehicle_layout.setVisibility(View.GONE);
+                    vehicle_details_layout.setVisibility(View.GONE);
+                }
+            }
+        });
 
-    public AddEmployeeFragment() {
-        // Required empty public constructor
-    }
+        rb_full_time.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    full_time_layout.setVisibility(View.VISIBLE);
+                else
+                    full_time_layout.setVisibility(View.GONE);
+            }
+        });
+        rb_intern.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    intern_layout.setVisibility(View.VISIBLE);
+                else
+                    intern_layout.setVisibility(View.GONE);
+            }
+        });
+        rb_part_time.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    part_time_layout.setVisibility(View.VISIBLE);
+                    rg_part_time.setVisibility(View.VISIBLE);
+                    part_time_layout.setVisibility(View.VISIBLE);
+                }
+                else {
+                    part_time_layout.setVisibility(View.GONE);
+                    part_time_layout.setVisibility(View.GONE);
+                    rg_part_time.setVisibility(View.GONE);
+                }
+            }
+        });
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddEmployeeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddEmployeeFragment newInstance(String param1, String param2) {
-        AddEmployeeFragment fragment = new AddEmployeeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_employee, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        return root;
     }
 }
