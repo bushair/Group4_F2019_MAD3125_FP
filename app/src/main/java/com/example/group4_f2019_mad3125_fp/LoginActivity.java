e04392552fa18f4febb5cdbfff8711873dd9a919
shiji703 <shijieugin3@gmail.com>
2019-12-10 20:46:21 -0500

    add employee

--- app/src/main/java/com/example/group4_f2019_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/example/group4_f2019_mad3125_fp/LoginActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+//import android.util.Log;
 import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
